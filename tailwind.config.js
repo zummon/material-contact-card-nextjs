df41498774a72652f2200b93be163a99dd5e5107
zummon <zummon.space@gmail.com>
2023-11-06 06:42:31 +0700

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,6 @@ module.exports = withMT({
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
